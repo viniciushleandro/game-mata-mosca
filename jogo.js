4795c20321dde4ec04f3e002fb0c2509bdee2898
Vinicius Leandro <vinilean43@gmail.com>
2022-01-24 23:03:43 -0300

    game completion

--- jogo.js
+++ jogo.js
@@ -2,6 +2,23 @@
 var altura = 0
 var largura = 0
 var vidas = 1
+var tempo = 15
+
+var criaMosquitoTempo = 1500
+
+var nivel = window.location.search
+nivel = nivel.replace('?', '')
+
+if(nivel === 'normal'){
+    //1500
+    criaMosquitoTempo = 1500
+} else if(nivel === 'dificil') {
+    //1000
+    criaMosquitoTempo = 1000
+} else if(nivel === 'chucknorris') {
+    //750
+    criaMosquitoTempo = 750
+}
 
 function ajustaTamanhoPalcoJogo(){
     altura = window.innerHeight
@@ -12,6 +29,20 @@ function ajustaTamanhoPalcoJogo(){
 
 ajustaTamanhoPalcoJogo()
 
+var cronometro = setInterval(function() {
+
+    tempo -= 1
+    if(tempo < 0){
+        clearInterval(cronometro)
+        clearInterval(criaMosca)
+        window.location.href = 'vitoria.html'
+    } else{
+        document.getElementById('cronometro').innerHTML = tempo
+    }
+    
+    
+}, 1000)
+
 function posicaoRandomica(){
 
     //remover a mosca anterior (caso exista)
@@ -20,9 +51,9 @@ function posicaoRandomica(){
 
 
         //console.log(vidas)
-        if(vidas > 3){
+        if(vidas > 2){
 
-            alert('Interromper o jogo')
+            window.location.href = 'fim_de_jogo.html'
         }else{
             document.getElementById('v' + vidas).src='imagens/coracao_vazio.png'
 
